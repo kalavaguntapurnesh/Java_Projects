4ce90175661b4145b10d060c0bfc29f396453b3d
Purnesh <114495054+Purnesh26@users.noreply.github.com>
2022-10-06 10:52:12 +0530

    Update Carpet_Cost_Calculator.java

--- Carpet_Cost_Calculator.java
+++ Carpet_Cost_Calculator.java
@@ -5,20 +5,24 @@ class Floor
 {
     double length;
     double width;
+    
+    //Creating a parameterized constructor for initializing the values.
     Floor(double length, double width)
     {
         if(length < 0)
         {
             this.length = 0;
         }
-        else {
+        else 
+        {
             this.length = length;
         }
         if(width < 0)
         {
             this.width = 0;
         }
-        else{
+        else
+        {
         this.width = width;
     }
 
@@ -31,6 +35,8 @@ class Floor
 class Carpet
 {
     double cost;
+    
+    //Creating a parameterized constructor for initializing the value.
     Carpet(double cost)
     {
         if(cost < 0)
@@ -51,6 +57,8 @@ class Calculator
 {
     Floor floor;
     Carpet carpet;
+    
+    //Creating a parameterized constructor for initializing the value.
     Calculator(Floor floor, Carpet carpet)
     {
         this.floor = floor;
@@ -65,15 +73,18 @@ public class CarpetCostCalculator {
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
+        //Scanner class for taking the inputs.
         System.out.println("Enter the length and width of carpet : ");
         double len = sc.nextDouble();
         double wid = sc.nextDouble();
         Floor floor = new Floor(len, wid);
+        //printing the total area of the carpet to find the total cost.
         System.out.println("The total area of carpet is " + floor.getArea());
         System.out.println("Enter the cost per square meter : ");
         double cos = sc.nextDouble();
         Carpet carpet = new Carpet(cos);
         Calculator calculator = new Calculator(floor, carpet);
         System.out.println("The total cost of the carpet is " + calculator.getTotalCost() + " rupees");
+        //printing the total cost.
     }
 }
